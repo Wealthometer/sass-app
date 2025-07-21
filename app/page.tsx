24650b80c016466dbc9e4df54988f6b7f4587855
Wealthometer <wealthhajoh87@gmail.com>
2025-07-21 22:03:10 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-export default function Home() {
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
-    <div>
-      <h1 className="text-2xl underline">
-        Welcome to my SaaS app
-      </h1>
-        <Button>Lets Get Started</Button>
-    </div>
-  );
+    <main>
+      <h1>Popular Companions</h1>
+
+        <section className="home-section">
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
+
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
+    </main>
+  )
 }
+
+export default Page
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+'use client';
+
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const navItems = [
+    { label:'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: 'my-journey' },
+]
 
 const NavItems = () => {
-  return (
-    <div>NavItems</div>
-  )
+    const pathname = usePathname();
+
+    return (
+        <nav className="flex items-center gap-4">
+            {navItems.map(({ label, href }) => (
+                <Link
+                    href={href}
+                    key={label}
+                    className={cn(pathname === href && 'text-primary font-semibold')}
+                >
+                    {label}
+                </Link>
+            ))}
+        </nav>
+    )
 }
 
 export default NavItems
